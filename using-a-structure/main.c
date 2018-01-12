aa055b859c6d5de7680e7a50d7f9424c6bd846ef
Rohan <10357179+rohan-molloy@users.noreply.github.com>
2018-01-12 16:40:19 +1300

    Create main.c

---
+++ using-a-structure/main.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+
+//
+// Create our structure containing attributes of the person.
+// Define a new type of "Person"
+//
+
+typedef struct person {
+	char *name;
+	char gender;
+	double weight;
+	double height;
+	unsigned int birthdate[3];
+} Person;
+
+//
+// Delcare our functions 
+// 
+void printPersion(Person);
+Person makeExamplePerson(void); 
+
+int main(int argc, char *argv[])
+{
+	Person johnsmith =  makeExamplePerson(); 
+	printPerson(johnsmith);
+	return 0; 
+}
+
+
+Person makeExamplePerson()
+{
+	Person p;
+	p.name = "John Smith";
+	p.gender = 'M';
+	p.weight = 75.7;
+	p.height = 1.75;
+	p.birthdate[0] = 10;
+	p.birthdate[1] = 12;
+	p.birthdate[2] = 1993;
+	return p;	
+}
+
+void printPerson(Person p)
+{
+	printf(
+	"Name:%s\nSex:%c\nWeight:%.2f\nHeight:%.2f\nDOB:%d-%d-%d\n",
+	       p.name,p.gender,p.weight,p.height,p.birthdate[0],
+	       p.birthdate[1],p.birthdate[2]
+	);
+}
