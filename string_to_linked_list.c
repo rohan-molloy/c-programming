5213ee180a5e74a735ee2e732743f5468f94f0f5
Rohan <10357179+rohan-molloy@users.noreply.github.com>
2019-08-10 20:50:09 +1200

    spacing

--- string_to_linked_list.c
+++ string_to_linked_list.c
@@ -9,46 +9,37 @@ struct linkednode
 };
 typedef struct linkednode linked_node;
 
+/* Function to print the linked list */
+int printlist(linked_node* );
+/* Function to calculate the number of elements in a string array */
+int arrsize(char *[] );
 /* Function to create linked node from string array */
 linked_node* createlist(char*[]);
 
-/* Function to calculate the number of elements in a string array */
-int arrsize(char *[] );
-
-/* Function to print the linked list */
-int printlist(linked_node* );
-
-
 int printlist(linked_node* list)
 {
-
    void *previous = NULL;
-   
     /* Check list is not null */
     if(!list)
      return 255;
-    
     /* Traverse until no next node exists */
     while(list != NULL)
     {
       printf("%s (%p)\n|\n",list->value,list);
       list = list->next;
     } 
-    
     printf("%p\n",list);
-
     return 0;
-
 }
-
 linked_node* createlist(char *array[])
 {
 	/* Pointers to head and current position*/
 	linked_node *head = NULL;
 	linked_node *currentnode = NULL;
 
-	// Iterate through the array, processing each element into a node
-	// We iterate backwards, so that the head of the final list is the first element in the array
+	/* Iterate through the array, processing each element into a node
+	 * We iterate backwards, so that the head of the final list 
+     * is the first element in the array */
 	for(int i = arrsize(array)-1; i>0; i--)
 	{
 		/* Create the pointer to the new (current node).
@@ -58,19 +49,15 @@ linked_node* createlist(char *array[])
 		currentnode = (linked_node*)malloc(sizeof(linked_node));
 		if(!currentnode) 
 			return NULL;
-		
 		/* Derefrence the ptr to struct 
-		 * and set its fields
-		 */
+		 and set its fields */
 		currentnode->value = array[i]; 
 		currentnode->next = head; 
-
 		/* Set head to point to current node*/
 		head = currentnode;
 	}
 	return head;
 }
-
 int arrsize(char *array[])
 {
 	int i = 0;
@@ -78,25 +65,19 @@ int arrsize(char *array[])
 		++i;
 	return i;
 }
-
 int main(int argc, char *argv[])
 {
-
 	if (argc < 2) 
     {
 		perror("Need at least 2 arguments!\n");
 		return 1;
 	}
-	
 	linked_node* linklist = createlist(argv);
-
 	if (!linklist) 
     {
 		perror("createList returned null!\n");
 		return 255;
 	}
-
 	printlist(linklist);
-
 	return 0;
 }
